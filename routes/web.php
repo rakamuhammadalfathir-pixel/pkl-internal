993d53e51545c803910a6529b5bbe4b6b4d124b1
RAka03 <rakamuhammadalfathir@gmail.com>
2025-12-15 11:23:28 +0700

    tentang

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/tentang', function () {
+    // ================================================
+    // Route::get() = Tangani HTTP GET request
+    // '/tentang'   = URL yang akan dihandle
+    // function     = Kode yang dijalankan saat URL diakses
+    // ================================================
+
+    return view('tentang');
+    // ↑ return view('tentang') = Tampilkan file tentang.blade.php
+    // ↑ Laravel akan mencari di: resources/views/tentang.blade.php
+});
