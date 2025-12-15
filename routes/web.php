854958849d6623ece5c3f48ba65bbcc7f9a7286a
RAka03 <rakamuhammadalfathir@gmail.com>
2025-12-15 11:42:25 +0700

    latihan 123

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,23 @@ Route::get('/tentang', function () {
     // ↑ return view('tentang') = Tampilkan file tentang.blade.php
     // ↑ Laravel akan mencari di: resources/views/tentang.blade.php
 });
+
+Route::get('/sapa/{nama}', function ($nama) {
+    // ↑ '/sapa/{nama}' = URL pattern
+    // ↑ {nama}         = Parameter dinamis, nilainya dari URL
+    // ↑ function($nama) = Parameter diterima di function
+
+    return "Halo, $nama! Selamat datang di Toko Online.";
+    // ↑ "$nama" = Variable interpolation (masukkan nilai $nama ke string)
+});
+
+Route::get('/kategori/{nama?}', function ($nama = 'Semua') {
+    // ↑ {nama?} = Tanda ? berarti parameter OPSIONAL
+    // ↑ $nama = 'Semua' = Nilai default jika parameter tidak diberikan
+
+    return "Menampilkan kategori: $nama";
+});
+
+Route::get('/produk/{id}', function ($id) {
+    return "Detail produk #$id";
+})->name('produk.detail');
